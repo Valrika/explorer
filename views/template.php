eb7040c306cbf807827695adad36795fdf7958c2
mvalma <66174692+mvalma@users.noreply.github.com>
2020-11-12 16:41:45 +0100

    maj 12-11 assets

--- views/template.php
+++ views/template.php
@@ -10,13 +10,16 @@ https://openclassrooms.com/fr/courses/4670706-adoptez-une-architecture-mvc-en-ph
         <meta charset="utf-8"/>
         <title><?= $title ?></title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <link rel="stylesheet" href="../assets/icon/style.css" />
+        <link rel="stylesheet" href="../public/assets/icon/style.css" />
     </head>
 
 
     <!--navbar sur le côté à gauche-->
-    <nav class="navbar fixed-top navbar-light bg-info">
-        <a href="#" class="navbar-brand">File-Explorer</a>
+    <nav class="navbar fixed-top .flex-column navbar-dark bg-light">
+        <a href="#" class="navbar-brand">Mes documents</a>
+        <a href="#" class="navbar-brand">Utilisateurs</a>
+        <a href="#" class="navbar-brand">Se connecter</a>
+        <a href="#" class="navbar-brand">Partage</a>
     </nav>
 
 
