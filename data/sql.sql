f69761b086b05f2aea5f0d1fe27b1c8e06868ab3
mvalma <66174692+mvalma@users.noreply.github.com>
2020-11-16 15:05:14 +0100

    try

---
+++ data/sql.sql
@@ -0,0 +1,2 @@
+INSERT INTO utilisateur.user (id, username, password, role) VALUES (1, 'karina', 'karina', 'admin');
+INSERT INTO utilisateur.user (id, username, password, role) VALUES (2, 'marika', 'marika', 'user');
