f724c78e4e69c6cce9cc93c643af1279b2f52aa6
ValerieEkoume <72264813+ValerieEkoume@users.noreply.github.com>
2020-11-16 09:41:49 +0100

    new file

---
+++ views/explorateur.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Connection;
+
+$pdo = (new Connection())->getPdo();
+
+$title = "mon site";
+$content = "content du site";
+require("template.php");
+?>
+
+    <!--se connecter et ou créer un compte en haut à droite-->
+
+    <div class"">
+    <div class="mt-5 container col-md-6">
+        <form action="" method="POST" enctype="multipart/form-data"><input type="file" name="file" class="form-control-file"><button type="submit" name="submit" class="btn btn-primary mt-3">Upload</button>
+        </form>
+    </div>
+    </div>
+
+
+<?php
+
+?>
