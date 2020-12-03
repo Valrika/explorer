1fb48b454bf2e3899f8fc3f7851edaefed483418
mvalma <66174692+mvalma@users.noreply.github.com>
2020-12-03 17:02:57 +0100

    documents

--- views/documents.php
+++ views/documents.php
@@ -10,13 +10,23 @@
     $pdo = (new Connection())->getPdo();
     $title = "Documents";
     ob_start();
-?>
+
 
 //répertoire dans l'explorateur de fichier
 //le nom d'utilisateur est lié à l'id
 //l'id, lié au nom d'utilisateur, est le nom du dossier dans le répertoire
 
-<?php
+/*
+$dir = "../files/3";
+
+if ($directory = opendir('3')) {
+    while(false !== $file = readdir($directory)) {
+        echo $file;
+    }
+    closedir($dossier);
+}
+*/
+
     $content = ob_get_clean();
     require("template.php");
 ?>
