9182e702ac6d36de81ad84cbcedabb18b0255719
mvalma <66174692+mvalma@users.noreply.github.com>
2020-12-08 17:04:29 +0100

    essais template

--- views/home_user.php
+++ views/home_user.php
@@ -6,16 +6,23 @@
 
     //ajouter non de l'utilisateur
     $title = "Bienvenue";
+
     ob_start();
 
+
+    //requête pour afficher le nom de l'utilisateur connecté
+    $sth=$pdo->prepare('SELECT * FROM user WHERE username = :username');
+    $sth->bindParam(':username', $username, PDO::PARAM_INT);
+    $sth->execute();
+    $username_echo=$sth->fetch()
 ?>
 
-    <!--afficher -->
+    <!--afficher nom de l'utilisateur connecté-->
 
 
     <div class="col-md-3 offset-md-4 form-div">
-
-                <h3>Bienvenue, Valérie </h3>
+<!--Mettre le bon nom-->
+                <h3>Bienvenue, <?php //echo$username_echo['username']; ?></h3>
 
         <div class="alert alert-warning">
                     en construction
